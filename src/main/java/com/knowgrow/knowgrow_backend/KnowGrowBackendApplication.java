2a6c516c1c6f9fe0f31036a90ab925343061695a
ShaliniD16 <shalinidatchina@gmail.com>
2025-01-05 17:05:05 +0530

    Initialize backend

---
+++ src/main/java/com/knowgrow/knowgrow_backend/KnowGrowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.knowgrow.knowgrow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowGrowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowGrowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowgrow/knowgrow_backend/KnowGrowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowgrow.knowgrow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowGrowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
